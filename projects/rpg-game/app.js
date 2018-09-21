33c5d5d3704f267ba5b5e1740800d67fdbb53754
Stephan <elondostephan@gmail.com>
2018-09-20 20:42:05 -0600

    ready for presentation

--- projects/rpg-game/app.js
+++ projects/rpg-game/app.js
@@ -1,61 +1,77 @@
 var rs = require("readline-sync");
 var player = {
     name: "",
-    hitPoints: 100,
-    attackPower: 15,
-    inventory: []
+    hitPoints: 0,
+    attackPower: 0,
+    inventory: [],
+    stillPlaying: true
 }
 
 function Enemy(){
-    var names = ["kings", "ada", "esoks", "clive", "sus", "eta"];
-    var items = ["Potion", "Tinder Password", "key", "syphilis"];
-    this.nam = ["kings", "ada", "esoks", "clive", "sus", "eta"];
-    // this.attackPower = [Math.floor(Math.random() * names.length)];
+    this.names = ["Ben", "Ken", "Stephan", "Dylan", "Anthony", "Nick"];
+    this.items = ["banana", "phone", "key", "laptop", "book"];
     this.hitPoints = Math.floor(Math.random() * 26 + 50);
     this.enemyAttackPower = Math.floor(Math.random() * 10 + 1);
 }
 
-player.name = rs.question("\nWelcome top RPG-land! What is your name?\n");
+function handleFight(attackPower){
+    let enemy = new Enemy(); 
+    if(attackPower > enemy.enemyAttackPower){
+            player.hitPoints = enemy.hitPoints;
+            player.inventory = enemy.items[Math.floor(Math.random() * enemy.items.length)];;
+            return `\nCongratulations! you killed the enemy`;
+            rs.question("\nPress any key to continue\n");          
+    }
+        else{
+                player.stillPlaying = false;
+                return `Sorry!! you were killed by the enemy`;
+        }
+} 
+  
+function handleWalk(){
+    var runsIntoEnemy = Math.floor((Math.random() * 18 )+ 1) ;
+    var enemy = new Enemy(); 
+    if(enemy.names[runsIntoEnemy] !== undefined){
+        console.log( `\nOh no, an Enemy with name ${ enemy.names[runsIntoEnemy]} appeared`);
+        var fight = rs.keyInYNStrict(`\nDo you stand and fight?\n`);
+        if (fight){
+            let fightPower = Math.floor((Math.random() * 10) + 1);
+            console.log(`\nYour fighting power is: ${fightPower}`);
+            return handleFight(fightPower);
+        } else{
+              var choice = rs.keyIn('\nyour only option left is to run. click "r" to run ', {limit: 'r'});
+              let runsIntoEnemy = Math.floor((Math.random() * 6) + 1);
+              if(enemy.names[runsIntoEnemy] !== undefined){
+                  player.stillPlaying = false;
+                  return `\nSorry! you couldn't escape The enemy killed you`
+              } else{
+                  player.stillPlaying = false;
+                  return `\nLucky you!! You managed to escape`
+              }
+          }
+    } else{
+          console.log("\nYou are safe!!");
+          let choice1 = rs.keyIn('click "w" to continue walking ', {limit: 'w'});
+          if(choice1){
+              return handleWalk();
+          }
+      }
+}
 
-console.log("\nHello, " + player.name + "!\n");
+player.name = rs.question("\nWelcome to RPG-land! What is your name?\n");
 
-var mainOptions = ["walk", "check stats"];
+console.log(`\nHello, ${player.name}!`);
 
-while(true){
-    var choice2 = rs.keyIn('walk or check stats? ', {limit: 'wc'});
-    //var choice = rs.keyInSelect(mainOptions, "\nYou may continue on your journey or view your player stats");
-    if(choice2 === "w"){
-        // console.log("walk")
-        var runsIntoEnemy = Math.floor((Math.random() * 5 )+ 1) ;
-        // var runsIntoEnemy = ((Math.random() * 6 )+ 1) ;//< .333333;
-         if(runsIntoEnemy){
-            var enemy = new Enemy();
-            
-            console.log("Oh no, an Enemy with name " + enemy.nam[runsIntoEnemy] + " appeared");
+var mainOptions = ["walk", "check stats", "e"];
 
-            var attacks = rs.keyInYNStrict("\nDo you stand and fight?\n");
-            if (attacks){
-                var attackPower = Math.floor((Math.random() * 10) + 1);
-                console.log("Your attack power is: " + attackPower +"\n");
-                var yes = rs.keyInYNStrict("\nDo you still want to attack?\n");
-                if(yes){
-                    if(attackPower > enemy.enemyAttackPower){
-                        console.log("Congratulations! you killed the enemy");
-                    }
-                    else{
-                        console.log("The enemy killed you")
-                    }
-                } else{
-                    console.log("Choose what you want to do:");
-                }
-            }
-            else{
-                console.log("no enemy");
-            }
-       }
-        
-    } else if(choice2 === 2){
-        console.log(player);
+while(player.stillPlaying === true){
+    let choice = rs.keyIn('\nclick "w" to walk, "c" to check stats or "e" to end game ', {limit: 'wce'});
+    if(choice === "w"){
+        console.log(handleWalk());
+    } else if(choice === "c"){
+        console.log(`name: ${player.name}, \nHit Point: ${player.hitPoints}, \nInventory: ${player.inventory}`);
         rs.question("\nPress any key to continue\n"); 
+    } else {
+        player.stillPlaying = false;
     }
 }
