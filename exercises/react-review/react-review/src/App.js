b9f80466e24e86f3b2891b39d9fb62373376cb4d
Stephan <elondostephan@gmail.com>
2018-10-02 09:43:34 -0600

    update

---
+++ exercises/react-review/react-review/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+function Box(props){
+    return(
+        <div>
+            <hr/>
+            <h1>{props.title}</h1>
+            <h3>{props.subtitle}</h3>
+            <a href={props.information}>{props.information}</a>
+        </div>
+    )
+}
+function App(){
+    const boxInfo = [{
+        title: "Intro to react",
+        subtitle: "practice",
+        information: "https://react.org"
+    },
+    {
+        title: "Intro to Angular",
+        subtitle: "practice",
+        information: "https://react.org"
+    },
+    {
+        title: "Intro to react",
+        subtitle: "practice",
+        information: "https://react.org"
+    },
+    {
+        title: "Intro to react",
+        subtitle: "practice",
+        information: "https://react.org"
+    },
+    {
+        title: "Intro to react",
+        subtitle: "practice",
+        information: "https://react.org"
+    },
+    {
+        title: "Intro to react",
+        subtitle: "practice",
+        information: "https://react.org"
+    }]
+const boxesInfo = [{...
+
+        function App(){
+            const boxComponents = boxesInfo.map((boxInfo, i) => <Box key={i} {...boxInfo} />)
+        return (
+                <div>
+                  {boxComponents}
+            </div>
+        )
+    }
+}]
+  
+export default App;
+    
