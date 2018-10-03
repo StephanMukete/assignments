67615ee938325753df71093739d0ceec8f1bbdb1
Stephan <elondostephan@gmail.com>
2018-10-03 09:36:51 -0600

    update

---
+++ exercises/state-intro/app.js
@@ -0,0 +1,17 @@
+const stateOfSteph = {
+    mood: "happy",
+    clothing: {
+        color: blue,
+        pants: jeans,
+        shoe: puma 
+    }
+}
+
+const newState = {
+    mood: "happy",
+    clothing: {
+        color: white,
+        pants: jeans,
+        shoe: puma 
+    }
+}
