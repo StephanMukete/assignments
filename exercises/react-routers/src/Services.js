88fde0dabfaaae7d627e97967779349fdb53df78
Stephan <elondostephan@gmail.com>
2018-10-07 23:20:53 -0600

    add

--- exercises/react-routers/src/Services.js
+++ exercises/react-routers/src/Services.js
@@ -1,10 +1,21 @@
-import React from React
-function About(){
-    return(
+import React from 'react'
+import services from "./services.json"
+
+function Services() {
+    const serviceCardComponents = services.map(service => (
+        <link to={'/services/${services.id}'}className ='service-card' key ={service.id}  >
+            <h3>{service.name}</h3>
+            <p>${service.price}</p>
+        </link>
+    ))
+    return (
         <section>
-            <h3>Welcome to home page</h3>
+            <h3>Services</h3>
+            <div className='service-card-list'>
+                {serviceCardComponents}
+            </div>
         </section>
     )
 }
 
-export default About
+export default Services;
