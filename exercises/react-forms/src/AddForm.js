3a4f4c800a660899ca96432c34aa97ec563a4a4d
Stephan <elondostephan@gmail.com>
2018-10-08 10:28:45 -0600

    first

--- exercises/react-forms/src/AddForm.js
+++ exercises/react-forms/src/AddForm.js
@@ -1,11 +1,13 @@
 function AddForm(props){
-    <form action="" onSubmit = {this.handleSubmit}>
-    <label htmlFor="">Username:</label>
-    <input name='username' onChange = {this.handleChange} type="text" value = {this.state.username}/>
-    <label htmlFor="">Password:</label>
-    <input name='password'type='password'onChange = {this.handleChange} value = {this.state.password}/>
-    <button>Submit</button>
-    </form> 
+    return(
+        <form action="" onSubmit = {this.handleSubmit}>
+            <label htmlFor="">Username:</label>
+            <input name='username' onChange = {this.handleChange} type="text" value = {this.state.username}/>
+            <label htmlFor="">Password:</label>
+            <input name='password'type='password'onChange = {this.handleChange} value = {this.state.password}/>
+            <button>Submit</button>
+            </form> 
+    )
 }
 // in form 
 // return (
