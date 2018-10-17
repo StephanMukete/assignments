9c428d33dec9d0c27e0aac4f695aa1f4b30cf1b1
Stephan <elondostephan@gmail.com>
2018-10-16 20:22:37 -0600

    update

--- projects/react-recipe-app/src/App.js
+++ projects/react-recipe-app/src/App.js
@@ -1,10 +1,10 @@
-
-
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from "react-router-dom";
 
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
+import About from "./components/About";
 
 const API_KEY = "d6a0d06d5a83a3cd351ebea8bd50d3cb";
 
@@ -15,10 +15,9 @@ class App extends Component {
   getRecipe = async (e) => {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=50`)
-
-    const data = await api_call.json();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
     
+    const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
   }
@@ -28,19 +27,18 @@ class App extends Component {
         const recipes = JSON.parse(json);
         this.setState({ recipes });
     }
-      }
-      componentUpdate = () => {
-        if(  localStorage.getItem("recipes") != null){ 
-         const json = localStorage.getItem("recipes");
-            const recipes = JSON.parse(json);
-            this.setState({ recipes });
-        }
-          }﻿
+  }
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
+          <nav><Link to="/about">About</Link></nav>
+          
         </header>
         <Form getRecipe={this.getRecipe} />
         <Recipes recipes={this.state.recipes} />
