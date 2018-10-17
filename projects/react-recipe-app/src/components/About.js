1e31343b91d02e89ef50a1cd70fac4f088816c80
Stephan <elondostephan@gmail.com>
2018-10-16 20:20:02 -0600

    update

---
+++ projects/react-recipe-app/src/components/About.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function About() {
+    return (
+           <div>  <body>
+             <p>the bpy</p>
+         </body>
+         <body>
+             <p>the bpy</p>
+         </body></div>     
+       
+    
+            
+        
+    )
+}
+
+export default About
+
+// import React from 'react';
+
+// const About = props => (
+//   <form onSubmit={props.getRecipe} style={{ marginBottom:"2rem" }}>
+//     <input className="form__input" type="text" name="recipeName" />
+//     <button className="form__button">Search</button>
+//   </form>
+// );
+
+// export default About;
