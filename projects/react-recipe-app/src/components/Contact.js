3686c56a1f9c8be8b6e8f62dea4e5a749c266613
Stephan <elondostephan@gmail.com>
2018-10-17 11:07:58 -0600

    update

---
+++ projects/react-recipe-app/src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Contact() {
+
+    return (
+           <div>  <body>
+             <p>the bpy</p>
+         </body>
+         <body>
+             <p>the bpy</p>
+         </body></div>     
+       
+    
+            
+        
+    )
+}
+
+export default Contact
