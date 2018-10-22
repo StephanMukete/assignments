9ab1095231244674b12acfdfc23ff3e4211b4b61
Stephan <elondostephan@gmail.com>
2018-10-21 23:00:18 -0600

    update

--- projects/react-recipe-app/src/index.js
+++ projects/react-recipe-app/src/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
+ import React from 'react';
+
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import $  from 'jquery';
 import Router from "./components/Router";
 import registerServiceWorker from './registerServiceWorker';
 
