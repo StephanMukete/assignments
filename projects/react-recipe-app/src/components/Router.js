a67943cc59b2518551ee66abf953c6bc9831de8e
Stephan <elondostephan@gmail.com>
2018-10-21 23:01:00 -0600

    update

--- projects/react-recipe-app/src/components/Router.js
+++ projects/react-recipe-app/src/components/Router.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "../App";
 import Recipe from "./Recipe";
 import About from "./About";
+import Contact from "./Contact";
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
       <Route path="/recipe/:id" component={Recipe} />
     </Switch>
   </BrowserRouter>
