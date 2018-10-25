8536c61dd1ab99343c17a8cece62160a3ea77448
Stephan <elondostephan@gmail.com>
2018-10-25 15:40:51 -0600

    update

--- projects/react-recipe-app/src/App.js
+++ projects/react-recipe-app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Link } from "react-router-dom";
 
+//components
+import Header from "./components/Header";
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
-import About from "./components/About";
+import Footer from "./components/Footer";
+
 
 const API_KEY = "d6a0d06d5a83a3cd351ebea8bd50d3cb";
 
@@ -16,14 +18,13 @@ class App extends Component {
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault();
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
-    
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
     console.log(this.state.recipes);
   }
   componentDidMount = () => {
-    if(  localStorage.getItem("recipes") != null){ 
-     const json = localStorage.getItem("recipes");
+    if( localStorage.getItem("recipes") != null ){ 
+        const json = localStorage.getItem("recipes");
         const recipes = JSON.parse(json);
         this.setState({ recipes });
     }
@@ -35,13 +36,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
-          <nav><Link to="/about">About</Link></nav>
-          
-        </header>
-        <Form getRecipe={this.getRecipe} />
-        <Recipes recipes={this.state.recipes} />
+        <Header />
+        <p></p>
+        <Form getRecipe={this.getRecipe} className="carousel-item" />
+        <Recipes recipes={this.state.recipes} className="carousel-item" />
+        <Footer />
       </div>
     );
   }
