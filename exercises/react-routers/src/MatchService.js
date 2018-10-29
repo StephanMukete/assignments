39eed06d823235f98b191a0b39f011bbf8fb7c89
Stephan <elondostephan@gmail.com>
2018-10-28 22:57:13 -0600

    update

--- exercises/react-routers/src/MatchService.js
+++ exercises/react-routers/src/MatchService.js
@@ -1,3 +1,12 @@
-function MatchService(match) {
-    const foundService = services.find(service => services.id === match.params.)
+import React from 'react';
+
+import services from "./services.json";
+
+import ServiceDetail from "./ServiceDetail";
+
+function MatchService({match}) {
+    const foundService = services.find(service => service.id === match.params.serviceId);
+    return <ServiceDetail {...foundService} />
 }
+
+export default MatchService;
