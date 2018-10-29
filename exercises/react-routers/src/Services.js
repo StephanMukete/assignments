ea5b54b8e08008bb0718971bcc7cac67c3ca24a4
Stephan <elondostephan@gmail.com>
2018-10-28 22:56:07 -0600

    update

--- exercises/react-routers/src/Services.js
+++ exercises/react-routers/src/Services.js
@@ -1,17 +1,21 @@
 import React from 'react'
-import services from "./services.json"
+
+import services from "./services.json";
+
+import { Link } from "react-router-dom";
+
 
 function Services() {
     const serviceCardComponents = services.map(service => (
-        <link to={'/services/${services.id}'}className ='service-card' key ={service.id}  >
+        <Link to={`/services/${service.id}`}className="service-card" key={service.id}>
             <h3>{service.name}</h3>
             <p>${service.price}</p>
-        </link>
+        </Link>
     ))
     return (
         <section>
             <h3>Services</h3>
-            <div className='service-card-list'>
+            <div className="service-card-list">
                 {serviceCardComponents}
             </div>
         </section>
