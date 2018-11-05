9778202af4db117a585286f5cc08838156d19bb4
Stephan <elondostephan@gmail.com>
2018-11-05 10:15:11 -0700

    update

---
+++ projects/full-stack/client/src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      <div>
+      <p>DevConnector</p>
+      <p>© 2018</p>
+      </div>
+      {/* Copyright &copy; {new Date().getFullYear()} DevConnector */}
+    </footer>
+  );
+};
