758c4dbab1c5808873a50882216fd8c26a23e0a4
Stephan <elondostephan@gmail.com>
2018-11-05 10:16:21 -0700

    update

--- projects/full-stack/server.js
+++ projects/full-stack/server.js
@@ -4,33 +4,31 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-// body parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use('/api/users', users); app.use('/api/profile', profile); app.use('/api/posts', posts)
-
-//DB config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//connect to mongoDB
+// Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-// passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-// passport config
-require('./config/passport')(passport)
+// Passport Config
+require('./config/passport')(passport);
 
-//use routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
@@ -38,5 +36,3 @@ app.use('/api/posts', posts);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.get('/', (req, res) => res.send("hello"));
